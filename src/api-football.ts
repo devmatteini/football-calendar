96bad7ad00c4d37ceacd7a138989c78ccf751dee
Cosimo Matteini <dev.matteini@gmail.com>
2023-09-09 03:24:43 +0200

    api-football: implement currentSeason endpoint

--- src/api-football.ts
+++ src/api-football.ts
@@ -8,6 +8,8 @@ import { formatErrors } from "@effect/schema/TreeFormatter"
 import * as Pretty from "@effect/schema/Pretty"
 import * as E from "@effect/data/Either"
 import axios from "axios"
+import * as ROA from "@effect/data/ReadonlyArray"
+import * as ORD from "@effect/data/Order"
 
 export type ApiFootballClient = {
     token: string
@@ -22,6 +24,27 @@ export const ApiFootballClientLive = Layer.effect(
         Effect.map((token) => ApiFootballClient.of({ token, baseUrl: "https://v3.football.api-sports.io" })),
     ),
 )
+
+export const currentSeason = (team: number) =>
+    ApiFootballClient.pipe(
+        Effect.flatMap((client) =>
+            F.pipe(
+                Effect.promise(() => get(client, "/teams/seasons", { team })),
+                Effect.flatMap((response) => decode(Response(S.number), response.data)),
+                Effect.flatMap((data) =>
+                    isResponseOk(data)
+                        ? Effect.succeed(data.response)
+                        : Effect.fail(new Error(`Response failed with ${ResponseErrorPrint(data.errors)}`)),
+                ),
+                Effect.flatMap(
+                    ROA.match({
+                        onEmpty: () => Effect.fail(new Error(`No seasons for team ${team}`)),
+                        onNonEmpty: (seasons) => F.pipe(seasons, ROA.max(ORD.number), Effect.succeed),
+                    }),
+                ),
+            ),
+        ),
+    )
 
 export const fixtures = (team: number, season: number, status: string) =>
     ApiFootballClient.pipe(
