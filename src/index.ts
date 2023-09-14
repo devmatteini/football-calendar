878bd663e7c791dd9b15b0bbdee28a88b672bb38
Cosimo Matteini <dev.matteini@gmail.com>
2023-09-15 00:02:58 +0200

    add LOG_LEVEL environment variable to change minimum log level

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import * as Config from "@effect/io/Config"
 import * as Effect from "@effect/io/Effect"
 import * as Logger from "@effect/io/Logger"
 import * as LogLevel from "@effect/io/LogLevel"
+import * as S from "@effect/schema/Schema"
+import * as E from "@effect/data/Either"
 import { calendarMatchesHandler } from "./calendar-matches-handler"
 import { CalendarMatchesHandlerDepsLive } from "./calendar-matches-handler-live"
 
@@ -20,10 +22,20 @@ const main = async () => {
             ),
         ),
         Effect.provideLayer(CalendarMatchesHandlerDepsLive),
-        Logger.withMinimumLogLevel(LogLevel.Debug),
+        Logger.withMinimumLogLevel(logLevel()),
         Effect.runPromise,
     )
 }
+
+const LogLevelSchema = S.union(S.literal("Debug"), S.literal("Info"), S.literal("Error"))
+const logLevel = () =>
+    F.pipe(
+        S.parseEither(LogLevelSchema)(process.env.LOG_LEVEL),
+        E.match({
+            onLeft: () => LogLevel.Info,
+            onRight: LogLevel.fromLiteral,
+        }),
+    )
 
 main().catch((e) => {
     console.error(e)
