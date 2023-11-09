f78d8a344b1794c7c81f8538bfdb2563b8fcd7c8
Cosimo Matteini <dev.matteini@gmail.com>
2023-11-10 00:10:44 +0100

    cleanup handler

--- src/football-match-events-handler.test.ts
+++ src/football-match-events-handler.test.ts
@@ -1,26 +1,23 @@
 import { expect, test, vi } from "vitest"
 import * as Effect from "effect/Effect"
 import * as F from "effect/Function"
-import * as Layer from "effect/Layer"
 import * as Exit from "effect/Exit"
 import { Deps, footballMatchEventsHandler } from "./football-match-events-handler"
-import { CalendarEvent, FootballMatch } from "./football-match-events"
-import exp from "constants"
+import { FootballMatch } from "./football-match-events"
 
 test("create football match event", async () => {
     const createCalendarEvent = vi.fn(() => Effect.unit)
 
-    const DepsTest = Layer.succeed(Deps, {
+    const DepsTest = Deps.of({
         loadMatchesByTeam: () => Effect.succeed([anyFootballMatch]),
         loadCalendarEventsByTeam: () => Effect.succeed([]),
-        updateCalendarEvent: () => Effect.unit,
         createCalendarEvent,
+        updateCalendarEvent: () => Effect.unit,
     })
 
     const result = await F.pipe(
-        // keep new line
         footballMatchEventsHandler(anyTeam),
-        Effect.provide(DepsTest),
+        Effect.provideService(Deps, DepsTest),
         Effect.runPromiseExit,
     )
 
@@ -36,15 +33,9 @@ const footballMatch = (id: number, date: Date): FootballMatch => ({
     awayTeam: "ANY_AWAY_TEAM",
     competition: "ANY_COMPETITION",
 })
-const calendarEvent = (matchId: number, startDate: Date, eventId?: string): CalendarEvent => ({
-    matchId,
-    startDate,
-    originalEvent: originalEvent(eventId || "9999"),
-})
 
 const date = (date: `${number}-${number}-${number}`, time?: `${number}:${number}`) =>
     new Date(`${date}T${time || "00:00"}Z`)
-const originalEvent = (id: string): CalendarEvent["originalEvent"] => ({ id })
 const anyTeam = 1
 const matchId1 = 1
 const anyFootballMatch = footballMatch(matchId1, date("2023-10-14", "15:00"))
--- src/football-match-events-handler.ts
+++ src/football-match-events-handler.ts
@@ -1,5 +1,4 @@
 import * as Effect from "effect/Effect"
-import * as Console from "effect/Console"
 import * as F from "effect/Function"
 import * as ROA from "effect/ReadonlyArray"
 import * as Match from "effect/Match"
@@ -76,20 +75,6 @@ BELOW YOU CAN FIND IMPLEMENTATION DETAILS THAT ARE NOT IMPORTANT FOR THE PURPOSE
 
 */
 
-const createOrUpdate = (create: Deps["createCalendarEvent"], update: Deps["updateCalendarEvent"]) =>
-    F.pipe(
-        Match.type<CreateOrUpdateEvent>(),
-        Match.tag("CREATE", (x) => create(x)),
-        Match.tag("UPDATE", (x) => update(x)),
-        Match.exhaustive,
-    )
-
-const toSummary = (events: readonly FootballMatchEvent[]) => ({
-    create: events.filter((x) => x._tag === "CREATE").length,
-    update: events.filter((x) => x._tag === "UPDATE").length,
-    nothingChanged: events.filter((x) => x._tag === "NOTHING_CHANGED").length,
-})
-
 const elaborateData = ({
     matches,
     calendarEvents,
@@ -98,21 +83,21 @@ const elaborateData = ({
     calendarEvents: readonly CalendarEvent[]
 }) => F.pipe(footballMatchEvents(matches, calendarEvents))
 
+type Summary = { create: number; update: number; nothingChanged: number }
+const toSummary = (events: readonly FootballMatchEvent[]): Summary => ({
+    create: events.filter((x) => x._tag === "CREATE").length,
+    update: events.filter((x) => x._tag === "UPDATE").length,
+    nothingChanged: events.filter((x) => x._tag === "NOTHING_CHANGED").length,
+})
+
 type CreateOrUpdateEvent = Exclude<FootballMatchEvent, { _tag: "NOTHING_CHANGED" }>
 const filterCreateOrUpdateEvents = (events: readonly FootballMatchEvent[]) =>
     ROA.filter(events, (x): x is CreateOrUpdateEvent => x._tag !== "NOTHING_CHANGED")
 
-const logMatchesAndCalendarEvents = ({
-    matches,
-    calendarEvents,
-}: {
-    matches: readonly FootballMatch[]
-    calendarEvents: readonly CalendarEvent[]
-}) => {
-    const events = calendarEvents.map((x) => ({
-        matchId: x.matchId,
-        startDate: x.startDate,
-        eventId: x.originalEvent?.id,
-    }))
-    return Console.log(JSON.stringify({ matches, calendarEvents: events }, null, 2))
-}
+const createOrUpdate = (create: Deps["createCalendarEvent"], update: Deps["updateCalendarEvent"]) =>
+    F.pipe(
+        Match.type<CreateOrUpdateEvent>(),
+        Match.tag("CREATE", (x) => create(x)),
+        Match.tag("UPDATE", (x) => update(x)),
+        Match.exhaustive,
+    )
