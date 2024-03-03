86d5eaa580219f8ccd1c5c51e522c0d5e47960bf
Cosimo Matteini <dev.matteini@gmail.com>
2024-03-03 15:07:42 +0100

    google-calendar: use effect generators

--- src/google-calendar.ts
+++ src/google-calendar.ts
@@ -22,29 +22,32 @@ const realPath = (path: string) => {
 
 export const GoogleCalendarClientLive = Layer.effect(
     GoogleCalendarClient,
-    F.pipe(
-        Config.all({
-            keyFile: Config.string("GOOGLE_CALENDAR_KEY_FILE").pipe(Config.map(realPath)),
-            calendarId: Config.string("GOOGLE_CALENDAR_ID"),
-        }),
-        Effect.bind("client", ({ keyFile }) =>
-            F.pipe(
-                Effect.tryPromise({
-                    try: () => auth.getClient({ keyFile, scopes: "https://www.googleapis.com/auth/calendar.events" }),
-                    catch: (e) => new Error(`Unable to authenticate with google api: ${e}`),
-                }),
-                Effect.map((client) => calendar({ version: "v3", auth: client })),
-            ),
-        ),
-        Effect.map(({ calendarId, client }) => GoogleCalendarClient.of({ calendarId, client })),
-    ),
+    Effect.gen(function* (_) {
+        const { calendarId, keyFile } = yield* _(
+            Config.all({
+                keyFile: Config.string("GOOGLE_CALENDAR_KEY_FILE").pipe(Config.map(realPath)),
+                calendarId: Config.string("GOOGLE_CALENDAR_ID"),
+            }),
+        )
+
+        const client = yield* _(
+            Effect.tryPromise({
+                try: () => auth.getClient({ keyFile, scopes: "https://www.googleapis.com/auth/calendar.events" }),
+                catch: (e) => new Error(`Unable to authenticate with google api: ${e}`),
+            }),
+            Effect.map((client) => calendar({ version: "v3", auth: client })),
+        )
+
+        return GoogleCalendarClient.of({ calendarId, client })
+    }),
 )
 
 // https://developers.google.com/calendar/api/v3/reference/events/list
 export const listEvents = (privateProperties: Record<string, string>, today: Date = new Date()) =>
-    F.pipe(
-        GoogleCalendarClient,
-        Effect.flatMap(({ client, calendarId }) =>
+    Effect.gen(function* (_) {
+        const { client, calendarId } = yield* _(GoogleCalendarClient)
+
+        const response = yield* _(
             Effect.tryPromise({
                 try: () =>
                     client.events.list({
@@ -59,15 +62,17 @@ export const listEvents = (privateProperties: Record<string, string>, today: Dat
                     }),
                 catch: (e) => new Error(`Unable to retrieve list of google calendar events: ${e}`),
             }),
-        ),
-        Effect.map((response) => response.data.items || []),
-    )
+        )
+
+        return response.data.items || []
+    })
 
 // https://developers.google.com/calendar/api/v3/reference/events/insert
 export const insertEvent = (event: GoogleCalendarEvent) =>
-    F.pipe(
-        GoogleCalendarClient,
-        Effect.flatMap(({ client, calendarId }) =>
+    Effect.gen(function* (_) {
+        const { client, calendarId } = yield* _(GoogleCalendarClient)
+
+        yield* _(
             Effect.tryPromise({
                 try: () =>
                     client.events.insert({
@@ -76,15 +81,15 @@ export const insertEvent = (event: GoogleCalendarEvent) =>
                     }),
                 catch: (e) => new Error(`Unable to insert google calendar event: ${e}`),
             }),
-        ),
-        Effect.asUnit,
-    )
+        )
+    })
 
 // https://developers.google.com/calendar/api/v3/reference/events/update
 export const updateEvent = (event: GoogleCalendarEvent) =>
-    F.pipe(
-        GoogleCalendarClient,
-        Effect.flatMap(({ client, calendarId }) =>
+    Effect.gen(function* (_) {
+        const { client, calendarId } = yield* _(GoogleCalendarClient)
+
+        yield* _(
             Effect.tryPromise({
                 try: () =>
                     client.events.update({
@@ -94,6 +99,5 @@ export const updateEvent = (event: GoogleCalendarEvent) =>
                     }),
                 catch: (e) => new Error(`Unable to update google calendar event: ${e}`),
             }),
-        ),
-        Effect.asUnit,
-    )
+        )
+    })
