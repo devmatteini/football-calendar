967f0bf9d8dc5600d38b336ee5509cc6fae6c6e2
Cosimo Matteini <dev.matteini@gmail.com>
2024-07-06 04:03:00 +0200

    create football calendar config file

---
+++ src/config-file.ts
@@ -0,0 +1,55 @@
+import * as Effect from "effect/Effect"
+import * as S from "@effect/schema/Schema"
+import * as TreeFormatter from "@effect/schema/TreeFormatter"
+import * as Context from "effect/Context"
+import * as Layer from "effect/Layer"
+import * as FileSystem from "@effect/platform/FileSystem"
+import * as O from "effect/Option"
+import * as F from "effect/Function"
+import * as os from "node:os"
+import * as path from "node:path"
+
+export const Team = S.TaggedStruct("Team", {
+    id: S.Int,
+    name: S.String,
+})
+export type Team = typeof Team.Type
+
+export const FootballCalendar = S.NonEmptyArray(Team)
+export type FootballCalendar = typeof FootballCalendar.Type
+
+export type FootballCalendarConfig = {
+    calendar: FootballCalendar
+}
+export const FootballCalendarConfig = Context.GenericTag<FootballCalendarConfig>("FootballCalendarConfig")
+
+export const FootballCalendarConfigLive = Layer.effect(
+    FootballCalendarConfig,
+    Effect.gen(function* (_) {
+        const fs = yield* _(FileSystem.FileSystem)
+
+        const configFile = path.join(configDir(), "football-calendar", "config.json")
+        const configExists = yield* _(fs.exists(configFile))
+        if (!configExists) return yield* _(Effect.fail(`Configuration file doesn't exists (${configFile})`))
+
+        const content = yield* _(fs.readFileString(configFile))
+        const json = yield* _(
+            Effect.try({
+                try: () => JSON.parse(content),
+                catch: (e) => `Unable to parse configuration file: ${e}`,
+            }),
+        )
+        const calendar = yield* _(
+            S.decodeUnknown(FootballCalendar)(json, { errors: "all" }),
+            Effect.mapError((e) => `Configuration file issues: ${TreeFormatter.formatErrorSync(e)}`),
+        )
+
+        return FootballCalendarConfig.of({ calendar })
+    }).pipe(Effect.orDie),
+)
+
+const configDir = () =>
+    F.pipe(
+        O.fromNullable(process.env.XDG_CONFIG_HOME),
+        O.getOrElse(() => path.join(os.homedir(), ".config")),
+    )
