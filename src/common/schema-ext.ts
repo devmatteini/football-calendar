e27dec09c62a6cba95c1eae9eaade2c6fbfc8e32
Cosimo Matteini <dev.matteini@gmail.com>
2025-04-05 18:38:15 +0200

    improve effect imports to be more tree shakeable

--- src/common/schema-ext.ts
+++ src/common/schema-ext.ts
@@ -1,10 +1,10 @@
 import * as F from "effect/Function"
 import * as Schema from "effect/Schema"
 import * as Effect from "effect/Effect"
-import { TreeFormatter } from "effect/ParseResult"
+import * as ParseResult from "effect/ParseResult"
 
 export const decode = <A, I>(schema: Schema.Schema<A, I>, input: unknown) =>
     F.pipe(
         Schema.decodeUnknown(schema)(input, { onExcessProperty: "ignore", errors: "all" }),
-        Effect.mapError((e) => new Error(`Schema decode error: ${TreeFormatter.formatErrorSync(e)}`)),
+        Effect.mapError((e) => new Error(`Schema decode error: ${ParseResult.TreeFormatter.formatErrorSync(e)}`)),
     )
