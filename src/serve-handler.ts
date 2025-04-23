8ddde170f255a8014728732791bbb8e032558777
Cosimo Matteini <dev.matteini@gmail.com>
2025-04-24 00:29:23 +0200

    first implementation of serve handler

--- src/serve-handler.ts
+++ src/serve-handler.ts
@@ -7,12 +7,54 @@ import * as HttpServerResponse from "@effect/platform/HttpServerResponse"
 import * as HttpMiddleware from "@effect/platform/HttpMiddleware"
 import * as NodeHttpServer from "@effect/platform-node/NodeHttpServer"
 import { createServer } from "node:http"
+import * as Context from "effect/Context"
+import * as Schema from "effect/Schema"
+import * as Array from "effect/Array"
+import * as ORD from "effect/Order"
+import { GoogleCalendarClient, GoogleCalendarClientLive, GoogleCalendarEvent, listEvents } from "./google-calendar"
+import * as SchemaExt from "./common/schema-ext"
+
+const CalendarEvent = Schema.Struct({
+    summary: Schema.String,
+    startDate: Schema.Date,
+})
+type CalendarEvent = typeof CalendarEvent.Type
+
+class NextMatchesDeps extends Context.Tag("NextMatchesDeps")<
+    NextMatchesDeps,
+    { loadMatches: Effect.Effect<CalendarEvent[]> }
+>() {}
+
+const MatchResponse = Schema.Struct({
+    summary: Schema.String,
+    startDate: Schema.Date,
+}).pipe(Schema.annotations({ identifier: "MatchResponse" }))
+
+const Response = Schema.Array(MatchResponse).pipe(Schema.annotations({ identifier: "Response" }))
+
+const byMostRecent = F.pipe(
+    ORD.Date,
+    ORD.mapInput((x: CalendarEvent) => x.startDate),
+)
 
 const router = HttpRouter.empty.pipe(
     HttpRouter.get(
         "/",
+        // TODO: add error handling
         Effect.gen(function* () {
-            return yield* HttpServerResponse.json({ ok: false })
+            const { loadMatches } = yield* NextMatchesDeps
+
+            const matches = yield* loadMatches
+            const nextMatches = F.pipe(
+                matches,
+                Array.sort(byMostRecent),
+                // TODO: make this value configurable?
+                Array.take(5),
+            )
+
+            const response = yield* Schema.encode(Response)(nextMatches)
+
+            return yield* HttpServerResponse.json(response)
         }),
     ),
 )
@@ -31,12 +73,47 @@ const app = router.pipe(
     HttpServer.withLogAddress,
 )
 
+const NextMatchesDepsLive = Layer.effect(
+    NextMatchesDeps,
+    Effect.gen(function* () {
+        const context = yield* Effect.context<GoogleCalendarClient>()
+
+        const validateCalendarEvent = (originalEvent: GoogleCalendarEvent) =>
+            F.pipe(
+                SchemaExt.decode(CalendarListEvent, originalEvent),
+                Effect.map(
+                    (validated): CalendarEvent => ({ summary: validated.summary, startDate: validated.start.dateTime }),
+                ),
+            )
+
+        const CalendarListEvent = Schema.Struct({
+            summary: Schema.String,
+            start: Schema.Struct({
+                dateTime: Schema.Date,
+            }),
+        })
+
+        const loadMatches = F.pipe(
+            listEvents({}),
+            Effect.flatMap(Effect.forEach(validateCalendarEvent)),
+            Effect.orDie,
+            Effect.provide(context),
+        )
+
+        return NextMatchesDeps.of({ loadMatches })
+    }),
+)
+
 const port = 6789
 const ServerLive = NodeHttpServer.layer(() => createServer(), { port })
 
-const ServeLive = F.pipe(app, Layer.provide(ServerLive))
+const ServeLive = F.pipe(
+    app,
+    Layer.provide(NextMatchesDepsLive),
+    Layer.provide(GoogleCalendarClientLive),
+    Layer.provide(ServerLive),
+)
 
 // TODO:
-//  1. Create JSON endpoint to return next X calendar events
 //  2. setup cron job(s) to sync calendar
 export const serveHandler = Layer.launch(ServeLive)
