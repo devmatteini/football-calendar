1c2d11c7189080e5162a199ff5195eb90ed368ec
Cosimo Matteini <dev.matteini@gmail.com>
2026-01-21 23:04:33 +0100

    football-calendar: retry failed background jobs + add logs
    This should solve the issue that after some days of running, the cron job stopped executing.
    It's probably due to that the effect fails and Effect.repeat stops the schedule after a failure

--- src/server/background-jobs.ts
+++ src/server/background-jobs.ts
@@ -1,11 +1,28 @@
 import * as Effect from "effect/Effect"
+import * as F from "effect/Function"
 import * as Cron from "effect/Cron"
 import * as Schedule from "effect/Schedule"
+import * as Duration from "effect/Duration"
 
 type BackgroundJob<A, E, R> = {
     job: Effect.Effect<A, E, R>
     cron: Cron.Cron
 }
 
+const retrySchedule = F.pipe(
+    // keep new line
+    Schedule.exponential(Duration.seconds(2)),
+    Schedule.compose(Schedule.recurs(3)),
+)
+
 export const registerBackgroundJob = <A, E, R>({ job, cron }: BackgroundJob<A, E, R>) =>
-    Effect.forkDaemon(Effect.repeat(job, Schedule.cron(cron)))
+    Effect.forkDaemon(
+        Effect.repeat(
+            F.pipe(
+                job,
+                Effect.retry(retrySchedule),
+                Effect.catchAllCause((cause) => Effect.logError("Background job failed after retries", cause)),
+            ),
+            Schedule.cron(cron),
+        ),
+    )
